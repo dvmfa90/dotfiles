3ad6730cee5ee821b40bebfefd35154e4d92ee7a
dvmfa90 <dvmfa90@gmail.com>
2021-08-09 15:53:39 +0100

    Mon Aug  9 15:53:40 2021

---
+++ fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+alias home='cd ~/'
+alias syncone='onedrive --synchronize'
+alias nn='nvim'
+alias ZZ='xdotool getwindowfocus windowkill'
+alias tls='tmux list-sessions'
+alias tks='tmux kill-server'
---
+++ installdotfiles.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cp -r dotfiles/fish ~/.config/
+cp -r dotfiles/neofetch ~/.config/
+
+rm -rf dotfiles
+
